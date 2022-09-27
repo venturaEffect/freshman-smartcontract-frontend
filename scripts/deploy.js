a7790025066776f4d3f6ef08e01044c8c0283f5f
Cesar Romero <zaesarpo@gmail.com>
2022-09-27 16:44:00 +0200

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Mood = await ethers.getContractFactory("Mood");
+    const mood = await Mood.deploy();
+  
+    console.log("Mood address:", mood.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
